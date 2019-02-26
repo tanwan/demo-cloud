f81b8bb0be2b5e00e18e8c3dfb7c57f289dd9f18
tanwan <99156629@qq.com>
2019-02-26 16:50:50 +0800

    ribbon测试类

---
+++ demo-service-server/src/main/java/com/lzy/demo/service/service/SampleSerivce.java
@@ -0,0 +1,12 @@
+/*
+ * Created by lzy on 2019-02-25 15:11.
+ */
+package com.lzy.demo.service.service;
+
+/**
+ * 调用feign的service
+ * @author lzy
+ * @version v1.0
+ */
+public interface SampleSerivce {
+}
