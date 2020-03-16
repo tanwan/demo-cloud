29f05b72b4a2581abea7f00b98bc33001b13faf7
lzy <lzytanwan@gmail.com>
2020-03-16 09:43:53 +0800

    调整项目结构

---
+++ gateway/demo-spring-gateway/src/main/java/com/lzy/demo/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+/*
+ * Created by lzy on 2020/3/14 3:14 PM.
+ */
+package com.lzy.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lzy
+ * @version v1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
